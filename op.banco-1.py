8b529a2525e6b3dcddb93ee60acfe0982ec6dca1
Jhonatan Paz <jhonatanpaaz@gmail.com>
2023-10-15 11:09:08 -0300

    Update op.banco-1.py

--- op.banco-1.py
+++ op.banco-1.py
@@ -10,7 +10,7 @@ menu = '''
 
 saldo = 0
 limite = 500
-extrato = ''
+extrato = []
 numero_saques = 0
 LIMITES_SAQUES = 3
 
@@ -26,9 +26,10 @@ while True:
         valor_minimo_deposito = 5
 
         if valor_deposito < valor_minimo_deposito:
-            print("Operação Invalida")
+            print("Operação Invalida, não é permitido depositos abaixo de R$ 5,00 ")
 
         saldo += valor_deposito
+        extrato.append(str(f'Deposito R$ {valor_deposito}'))
 
         print(f'o saldo é: R$ {saldo} \n')
         print('Deseja fazer outra operação?')
@@ -38,8 +39,34 @@ while True:
     elif opcao == '2':
         print('saque')
 
+        valor_saque = float(input('digite o valor de saque: R$ '))
+
+        if  limite < valor_saque:
+            print('Operação invalida, não é permitido saque acima de R$ 500,00')
+        elif saldo < valor_saque:
+            print('saldo insuficiente')
+            print(f'O valor disponivel é R$ {saldo}')
+
+        elif numero_saques < LIMITES_SAQUES:
+            saldo -= valor_saque
+            numero_saques += 1
+            #print(numero_saques)
+            extrato.append(str(f'Saque R$ {valor_saque}'))
+            print(saldo)
+        else:
+            print('O numero de saque excede o valor de saques permitidos') #mudar pra uma condição elif pra colocar outro comando no else
+
+        print('Deseja fazer outra operação?')            
+       
+
     elif opcao == '3':
         print('exibir extrato')
+
+        for item in extrato:
+            print(item)
+            
+        print(f'Saldo disponível {saldo}')
+
 
     elif opcao == '4':
         break
